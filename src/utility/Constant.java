7f0ae2a9573ba60c2e4add85fdee79be09032fc6
Sylvia Lawrence <sylvia.melba@gmail.com>
2016-06-20 04:07:56 -0700

    changed the variables

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -5,10 +5,10 @@ public class Constant {
 	/*Declare Variables*/
 	//Variables are declared as public static so that they can be called in any other methods without instantiate the class.
 	//Variables are declared a final, so that they cannot be changed during the execution.
-	public static final String listname = "List creation 18";
-	public static final String prospectname = "Prospect creation 11";
-	public static final String prospectemail = "prospect11@domain.com";
+	public static final String listname = "SylviaLawrence List";
+	public static final String prospectname = "SylviaLawrence Prospect";
+	public static final String prospectemail = "sylviaprospect@domain.com";
 	public static final int prospectscore = 100; 
-	public static final String emailName = "Email 7";
+	public static final String emailName = "SylviaLawrence Email";
 	
 }
